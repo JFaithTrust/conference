6fed56790ebeb05beadbd7a9e2d58e5ebb271609
QurbonbekovaSabira <sarakim2224@gmail.com>
2024-10-07 10:43:55 +0500

    card

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -6,4 +6,24 @@ export interface UserType {
     phoneNumber: string;
     userStatus: string;
 }
-
+export interface ConferenceType {
+    id: number;
+    name: string;
+    startsAt?: string;
+    endsAt?: string | Date;
+    deadlineForThesis: string;
+    cost?: string;
+    description?: string;
+    address?: string;
+    requirements?: string;
+    newApplicationsCount?: number;
+    images:string;
+    owner?: {
+      id: number;
+      fullName: string;
+      email: string;
+      username: string;
+      role: string;
+      phoneNumber: string;
+    };
+  }
