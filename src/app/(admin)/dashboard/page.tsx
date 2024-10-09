bf6883e2f70481cf1ad286fa7d52e98a0b3aa671
QurbonbekovaSabira <sarakim2224@gmail.com>
2024-10-09 14:59:37 +0500

    chart

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -4,21 +4,18 @@ import Sidebar from "@/components/layout/side-bar";
 import WeekCalendar from "@/components/custom/week-calendar";
 import { TaskTodayCard } from "@/components/cards/task-today.card";
 import { ScrollArea } from "@/components/ui/scroll-area";
-
-
+import ActivityChart from "@/components/custom/ActivityChart";
 const DashboardPage = () => {
   const [open, setOpen] = useState(true);
 
   return (
     <div className="flex bg-indigo-50 justify-between w-full">
       <Sidebar open={open} setOpen={setOpen} />
-      <div className="max-w-[60%] w-full ">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
-          adipisci impedit a odio alias et quisquam laudantium corrupti libero,
-          eveniet officiis pariatur omnis tempore quibusdam illo recusandae quia
-          inventore sed.
-        </p>
+      <div className="max-w-[60%] w-full px-5 pt-10">
+        <div className="flex items-center gap-8">
+          <div className="w-[250px] h-full border-2 border-primary">Lorem ipsum dolor sit.</div>
+          <ActivityChart />
+        </div>
       </div>
       <ScrollArea className=" h-[100vh] pr-2 rounded-md border">
         <div className="flex flex-col bg-white p-4 space-y-4 ">
---
+++ src/components/custom/ActivityChart.tsx
@@ -0,0 +1,69 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  Bar,
+} from "recharts";
+
+const chartData = [
+  { name: "S", value: 1.7 },
+  { name: "M", value: 2 },
+  { name: "T", value: 1.4 },
+  { name: "W", value: 3 },
+  { name: "T", value: 1.8 },
+  { name: "F", value: 2.4 },
+  { name: "S", value: 1.9 },
+];
+
+const ActivityChart = () => {
+  const CustomTooltip = ({
+    payload,
+    label,
+    active,
+  }: {
+    payload: any;
+    label: any;
+    active: any;
+  }) => {
+    if (active) {
+      return (
+        <div className="w-16 h-10 rounded-md flex items-center border-[#9CA3AF] justify-center border bg-white">
+          <p className="label">{`${payload[0].value}`}</p>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="w-full h-40 bg-gray-50 p-4 rounded-lg">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart data={chartData}>
+          <XAxis
+            dataKey="name"
+            tick={{ fontSize: 12, fontWeight: 600, fill: "#9CA3AF" }} // Styling x-axis labels Remove tick line
+          />
+          <YAxis
+            domain={[1, 3]} // Set min and max values
+            tick={{ fontSize: 12, fontWeight: 600, fill: "#9CA3AF" }} // Styling y-axis labels
+          />
+          <Tooltip
+            content={<CustomTooltip />}
+            contentStyle={{ borderRadius: "8px", padding: "10px" }}
+          />
+          <Line
+            type="monotone"
+            dataKey="value"
+            stroke="#000"
+            strokeWidth={3}
+            dot={false} // Removes points on the line
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default ActivityChart;
