6040f96f64e8b946e30ba5b4b36e4fea1adef931
user <muhammadmustafo059@gmail.com>
2024-10-18 17:25:38 +0500

    Fields page started

--- src/app/(admin)/dashboard/conferences/fields/page.tsx
+++ src/app/(admin)/dashboard/conferences/fields/page.tsx
@@ -1,7 +1,28 @@
+import { router } from "next/client";
+import { FaSearch } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa6";
+
+
 const ConferenceFieldsPage = () => {
   return (
     <div>
-        <h1>Conference Fields Page</h1>
+        <div className="flex flex-row justify-between mt-10 mx-16">
+  <div>
+    <button 
+    className="flex flex-row items-center px-2 py-1 rounded-md bg-indigo-500 font-semibold text-white"
+
+    >
+      <FaPlus className="mr-1" /> Yaratish
+    </button>
+  </div>
+  <div>
+    <button className="flex flex-row items-center px-2 py-1 rounded-md bg-indigo-500 font-semibold text-white">
+      <FaSearch className="mr-1" /> Qidirish
+    </button>
+  </div>
+</div>
+
+
     </div>
   );
 }
