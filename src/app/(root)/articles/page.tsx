e37c1b8e0a8f59ba710a8443e757acdf673d402c
user <muhammadmustafo059@gmail.com>
2024-11-22 15:10:34 +0500

    My-articles page is ready. Footer with new color.

--- src/app/(root)/articles/page.tsx
+++ src/app/(root)/articles/page.tsx
@@ -1,24 +1,24 @@
-
-
 import columns from "@/app/(root)/articles/my-articles-page";
-import { DataTable } from "@/components/custom/data-table";
+import {DataTable} from "@/components/custom/data-table";
 import {getApplicationByConferenceId} from "@/lib/actions/application.action";
 
 
 const ArticlesPage = async (conferenceId: string) => {
- const [applicationData] = await Promise.all([getApplicationByConferenceId(conferenceId)])
-  return (
-    <div className={"container"}>
-        <h1>Mening maqolalarim</h1>
-      <DataTable
-        columns={columns}
-        data={applicationData}
-        hasPagination={true}
-        hasFilter={true}
-        route={"/articles"}
-      />
-    </div>
-  );
+    const [applicationData] = await Promise.all([getApplicationByConferenceId(conferenceId)])
+    return (
+        <div className={"container mt-6 space-y-6"}>
+            <h1 className="text-3xl font-semibold text-gray-800">
+                Mening maqolalarim
+            </h1>
+            <DataTable
+                columns={columns}
+                data={applicationData}
+                hasPagination={true}
+                hasFilter={true}
+                route={"/articles"}
+            />
+        </div>
+    );
 };
 
 export default ArticlesPage;
