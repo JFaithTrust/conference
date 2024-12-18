544cb05703586c144694e65efc3547cd666779ab
Husniddin <husniddinzoirjonov@gmail.com>
2024-12-18 17:59:16 +0500

    hero section

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images.unsplash.com',
-            }
-        ]
-    },
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: true,
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.mikro-makro.net",
+      },
+    ],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
--- src/lib/actions/conference.action.ts
+++ src/lib/actions/conference.action.ts
@@ -1,91 +1,91 @@
-"use server"
+"use server";
 
-import {revalidatePath} from "next/cache";
+import { revalidatePath } from "next/cache";
 
-import {getCookieToken} from "@/lib/actions/auth.action";
-import {IConference} from "@/types";
+import { getCookieToken } from "@/lib/actions/auth.action";
+import { IConference } from "@/types";
 
 const URL = process.env.NEXT_PUBLIC_GLOBAL_API_URL;
 
-export async function getAllConferences(){
-    try {
-        const response = await fetch(`${URL}/conference/all`, {
-            method: "GET",
-            next: {
-                tags: ["conference"]
-            }
-        })
+export async function getAllConferences() {
+  try {
+    const response = await fetch(`${URL}/conference/landing`, {
+      method: "GET",
+      next: {
+        tags: ["conference"],
+      },
+    });
 
-        const data: IConference[] = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
+    const data: IConference[] = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function getConferenceById(id: string) {
-    try {
-        const response = await fetch(`${URL}/conference/${id}`, {
-            method: "GET",
-            cache: "no-store"
-        })
+  try {
+    const response = await fetch(`${URL}/conference/${id}`, {
+      method: "GET",
+      cache: "no-store",
+    });
 
-        const data: IConference = await response.json();
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
+    const data: IConference = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 interface PostConferenceProps {
-    name: string;
-    startsAt: Date;
-    endsAt: Date;
-    deadlineForThesis: Date;
-    cost: string;
-    description: string;
-    requirements: string;
-    address: string;
-    directions: number[];
+  name: string;
+  startsAt: Date;
+  endsAt: Date;
+  deadlineForThesis: Date;
+  cost: string;
+  description: string;
+  requirements: string;
+  address: string;
+  directions: number[];
 }
 
 export async function postConference(data: PostConferenceProps) {
-    const token = await getCookieToken();
+  const token = await getCookieToken();
 
-    try {
-        const res = await fetch(`${URL}/conference`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify(data)
-        })
+  try {
+    const res = await fetch(`${URL}/conference`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(data),
+    });
 
-        console.log(await res.json())
-        revalidatePath("/dashboard/conferences/all")
-        return "ok";
-    } catch (error) {
-        console.log(error);
-    }
+    console.log(await res.json());
+    revalidatePath("/dashboard/conferences/all");
+    return "ok";
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function putConference(id: number, values: PostConferenceProps) {
-    const token = await getCookieToken();
+  const token = await getCookieToken();
 
-    try {
-        await fetch(`${URL}/conference/${id}`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${token}`,
-            },
-            body: JSON.stringify(values)
-        })
+  try {
+    await fetch(`${URL}/conference/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(values),
+    });
 
-        revalidatePath("/dashboard/conferences/all")
-        return "ok";
-    } catch (error) {
-        console.log(error);
-    }
+    revalidatePath("/dashboard/conferences/all");
+    return "ok";
+  } catch (error) {
+    console.log(error);
+  }
 }
